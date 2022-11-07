df4fd8811691af7433e7781fc08753d69c4c5fb6
Documenter.jl <documenter@juliadocs.github.io>
2022-11-07 18:52:16 +0000

    build based on 308987a

---
+++ latest/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Curious","category":"page"},{"location":"#Curious","page":"Home","title":"Curious","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Curious.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Curious]","category":"page"}]
+}
